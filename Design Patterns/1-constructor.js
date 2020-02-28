32d4436f7bbfa70b341059526c0a1810f6e35ce1
vivekgondalia <vgondalia@drintl.com>
2020-02-28 11:24:40 -0500

    Object.prototype to share single instance of methods between all objects

--- Design Patterns/1-constructor.js
+++ Design Patterns/1-constructor.js
@@ -62,3 +62,48 @@ Object.defineProperties( newObject, {
     }
    
   });
+
+
+  function Car( model, year, miles ) {
+ 
+    this.model = model;
+    this.year = year;
+    this.miles = miles;
+   
+    this.toString = function () {
+      return this.model + " has done " + this.miles + " miles";
+    };
+  }
+   
+  // Usage:
+   
+  // We can create new instances of the car
+  var civic = new Car( "Honda Civic", 2009, 20000 );
+  console.log( civic.toString() );
+
+  //PROBLEM
+  /**
+   * One is that it makes inheritance difficult and the other is that functions such as toString() are redefined for each of the new objects created using the Car constructor. This isn't very optimal as the function should ideally be shared between all of the instances of the Car type.
+   */
+
+   //SOLUTION
+   function Car( model, year, miles ) {
+ 
+    this.model = model;
+    this.year = year;
+    this.miles = miles;
+   
+  }
+   
+  // Note here that we are using Object.prototype.newMethod rather than
+  // Object.prototype so as to avoid redefining the prototype object
+  Car.prototype.toString = function () {
+    return this.model + " has done " + this.miles + " miles";
+  };
+   
+  // Usage:
+  var civic = new Car( "Honda Civic", 2009, 20000 );
+  console.log( civic.toString() );
+
+  //RESULT
+  //a single instance of toString() will now be shared between all of the Car objects.
