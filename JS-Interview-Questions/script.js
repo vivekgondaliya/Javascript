a63e8b322fed2cf79d8edd61cd1da084854d9ff8
vivekgondalia <vgondalia@drintl.com>
2020-03-06 16:03:14 -0500

    js interview questions

---
+++ JS-Interview-Questions/script.js
@@ -0,0 +1,10 @@
+/**
+ * Q) Why is it called a Ternary expression, what does the word "Ternary" indicate?
+ * 
+ * Ternary means it involves 3 things and strings them all on one line so it's harder to read. :) 
+ */
+
+  /**
+   * Event Delegation vs Event Bubbling/Propogation
+   * Events on parent or children is registered by the other.
+   */
